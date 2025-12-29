1a9af49b2aeb2777ad85c850b9c9b0ee3b2171b8
Sajid Shaik <sajid896969@gmail.com>
2025-12-29 13:29:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+const authRoutes = require("./routes/auth");
+const menuRoutes = require("./routes/menu");
+const orderRoutes = require("./routes/orders");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: "restaurant_secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use("/", authRoutes);
+app.use("/menu", menuRoutes);
+app.use("/orders", orderRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
