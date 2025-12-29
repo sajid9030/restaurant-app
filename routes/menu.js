bbc88d77c94a9143f112c34acc1a859611549131
Sajid Shaik <sajid896969@gmail.com>
2025-12-29 13:34:57 +0530

    Update menu.js

--- routes/menu.js
+++ routes/menu.js
@@ -4,9 +4,20 @@ const db = require("../config/db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  if (!req.session.cart) req.session.cart = [];
+
   db.query("SELECT * FROM menu", (err, items) => {
-    res.render("menu", { items });
+    res.render("menu", { items, cart: req.session.cart });
   });
+});
+
+router.post("/add", (req, res) => {
+  const { id, name, price } = req.body;
+
+  if (!req.session.cart) req.session.cart = [];
+
+  req.session.cart.push({ id, name, price });
+  res.redirect("/menu");
 });
 
 module.exports = router;
